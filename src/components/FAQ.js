f7a95ee798b9a70371305d968c019537aa8dcc6c
F.Lora <federicalora94@gmail.com>
2023-10-01 00:49:09 +0200

    feat: Created GetStarted, RepoRadar, and FAQ page basic structure & linked the navbar links to their respective page.
    Linked to: [Issue #9](https://github.com/federicalora94/orderbird-repo-radar/issues/9)
    
    Signed-off-by: F.Lora <federicalora94@gmail.com>

---
+++ src/components/FAQ.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FAQ() {
+    return (
+        <div>
+            <h1>FAQ Page</h1>
+            <p>These are the Frequently Asked Questions.</p>
+        </div>
+    );
+}
+
+export default FAQ;
---
+++ src/components/GetStarted.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GetStarted() {
+    return (
+        <div>
+            <h1>Get Started Page</h1>
+            <p>Welcome to the Get Started page.</p>
+        </div>
+    );
+}
+
+export default GetStarted;
