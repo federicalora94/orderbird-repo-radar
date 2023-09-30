6eaf9182e16e956176c61d399b037a6240e0e700
F.Lora <federicalora94@gmail.com>
2023-09-30 23:24:18 +0200

    feat: Initial draft of the application skeleton Outlined the initial draft of the application by creating fundamental components, such as Header.js, Main.js and Footer.js
    Related to: [Issue #8](https://github.com/federicalora94/orderbird-repo-radar/issues/8)
    
    Signed-off-by: F.Lora <federicalora94@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// Core React imports
+import React from 'react';
 
+// Application-specific component imports
+import Header from './components/Header';
+import Main from './components/Main';
+import {Footer} from './components/Footer';
+
+/**
+ * App Component
+ *
+ * This is the root component of the application. It orchestrates the layout
+ * by composing the Header, Main, and Footer components.
+ *
+ * @returns {JSX.Element} The rendered component
+ */
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            {/*
+        Wrapper class for the main layout components.
+        Purpose: To group Header, Main, and Footer for potential layout styling.
+      */}
+            <div className="app-wrapper">
 
+                {/*
+          Header Section
+          Includes: Navigation bar, any top-level application information
+        */}
+                <Header/>
+
+                {/*
+          Main Content Section
+          Includes: Form for GitHub repository search, Results display area
+        */}
+                <Main/>
+
+                {/*
+          Footer Section
+          Includes: Footer information, copyright, etc.
+        */}
+                <Footer/>
+
+            </div>
+        </div>
+    );
+}
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <main className="main-content p-4 flex flex-col items-center justify-center">
+            {/* TODO: Implement Routing and Component Loading - Linked to issue "Routing" */}
+        </main>
+    );
+}
+
+export default Main;
