cfc3f308ee802e4986dadf6f8e5e9179e53a6691
F.Lora <federicalora94@gmail.com>
2023-10-01 19:53:04 +0200

    Feature: Integrated RecentSearches into Dashboard and Updated Layout
    Related to issue:[14](https://github.com/federicalora94/orderbird-repo-radar/issues/14)
    
    Signed-off-by: F.Lora <federicalora94@gmail.com>

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -2,14 +2,17 @@ import React from 'react';
 import RepoRadar from '../RepoRadar';
 import SidePanel from '../SidePanel';
 import RecentSearches from '../RecentSearches';
-import {RecentSearchProvider} from '../RecentSearchContext';
+import { RecentSearchProvider } from '../../utils/RecentSearchContext';
 
 function Dashboard() {
     return (
         <RecentSearchProvider>
             <div className="flex gap-5" id="dashboard-wrapper">
-                <SidePanel/>
-                <RepoRadar/>
+                <SidePanel />
+                <div className="flex flex-col gap-1">
+                    <RepoRadar />
+                    <RecentSearches />
+                </div>
             </div>
         </RecentSearchProvider>
     );
