c2289ad176a10a12468dd057db9c48e3a70cad4e
F.Lora <federicalora94@gmail.com>
2023-10-02 02:48:04 +0200

    refactor: Make Side Panel responsive - Introduced a horizontal layout for small screens, displaying only icons at the top of the card. - Maintained the vertical layout with text labels and icons for large screens.
    Related to issue: [#21](https://github.com/federicalora94/orderbird-repo-radar/issues/21)
    
    Signed-off-by: F.Lora <federicalora94@gmail.com>

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -15,58 +15,75 @@ import {
     InboxIcon,
     PowerIcon,
 } from "@heroicons/react/24/solid";
-import RepoRadar from "./RepoRadar";
 
 function SidePanel() {
     return (
-        <Card className="h-[calc(80vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 rounded-none bg-transparent">
-            <div className="mb-2 p-4">
-                <Typography variant="h5" color="blue-gray">
-                    Repo Radar
-                </Typography>
+        <div>
+            {/* For large screens */}
+            <div className="hidden lg:block">
+                <Card
+                    className="h-[calc(80vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 rounded-none bg-transparent">
+                    <div className="mb-2 p-4">
+                        <Typography variant="h5" color="blue-gray">
+                            Repo Radar
+                        </Typography>
+                    </div>
+                    <List>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <PresentationChartBarIcon className="h-5 w-5"/>
+                            </ListItemPrefix>
+                            <span className="hidden sm:inline">Dashboard</span>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <ShoppingBagIcon className="h-5 w-5"/>
+                            </ListItemPrefix>
+                            <span className="hidden sm:inline">Recent Searches</span>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <InboxIcon className="h-5 w-5"/>
+                            </ListItemPrefix>
+                            <span className="hidden sm:inline">Inbox</span>
+                            <ListItemSuffix>
+                                <Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full"/>
+                            </ListItemSuffix>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <UserCircleIcon className="h-5 w-5"/>
+                            </ListItemPrefix>
+                            <span className="hidden sm:inline">Profile</span>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <Cog6ToothIcon className="h-5 w-5"/>
+                            </ListItemPrefix>
+                            <span className="hidden sm:inline">Settings</span>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <PowerIcon className="h-5 w-5"/>
+                            </ListItemPrefix>
+                            <span className="hidden sm:inline">Log Out</span>
+                        </ListItem>
+                    </List>
+                </Card>
             </div>
-            <List>
-                <ListItem>
-                    <ListItemPrefix>
-                        <PresentationChartBarIcon className="h-5 w-5" />
-                    </ListItemPrefix>
-                    Dashboard
-                </ListItem>
-                <ListItem>
-                    <ListItemPrefix>
-                        <ShoppingBagIcon className="h-5 w-5" />
-                    </ListItemPrefix>
-                    Recent Searches
-                </ListItem>
-                <ListItem>
-                    <ListItemPrefix>
-                        <InboxIcon className="h-5 w-5" />
-                    </ListItemPrefix>
-                    Inbox
-                    <ListItemSuffix>
-                        <Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full" />
-                    </ListItemSuffix>
-                </ListItem>
-                <ListItem>
-                    <ListItemPrefix>
-                        <UserCircleIcon className="h-5 w-5" />
-                    </ListItemPrefix>
-                    Profile
-                </ListItem>
-                <ListItem>
-                    <ListItemPrefix>
-                        <Cog6ToothIcon className="h-5 w-5" />
-                    </ListItemPrefix>
-                    Settings
-                </ListItem>
-                <ListItem>
-                    <ListItemPrefix>
-                        <PowerIcon className="h-5 w-5" />
-                    </ListItemPrefix>
-                    Log Out
-                </ListItem>
-            </List>
-        </Card>
+
+            {/* For small screens */}
+            <div
+                className="lg:hidden flex justify-center items-center bg-transparent pt-5 px-10 gap-4 text-blue-gray">
+                <PresentationChartBarIcon className="h-5 w-5" fill='#3c5058'/>
+                <ShoppingBagIcon className="h-5 w-5" fill='#3c5058'/>
+                <InboxIcon className="h-5 w-5" fill='#3c5058'/>
+                <UserCircleIcon className="h-5 w-5" fill='#3c5058'/>
+                <Cog6ToothIcon className="h-5 w-5" fill='#3c5058'/>
+                <PowerIcon className="h-5 w-5" fill='#3c5058'/>
+            </div>
+        </div>
     );
 }
+
 export default SidePanel;
