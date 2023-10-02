dc616bcabcb6ef5e5032117aad62733642bc0459
F.Lora <federicalora94@gmail.com>
2023-10-02 03:36:27 +0200

    refactor: Minor adjustments for enhanced responsiveness Related to issue: [#21](https://github.com/federicalora94/orderbird-repo-radar/issues/21)
    Signed-off-by: F.Lora <federicalora94@gmail.com>

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -2,7 +2,8 @@ import { StickyNavbar } from './StickyNavbar';  // Adjust the path as needed
 
 function Header() {
     return (
-        <header className="sticky top-0">
+
+        <header className="sticky top-0 z-50  ">
             <StickyNavbar />
         </header>
     );
